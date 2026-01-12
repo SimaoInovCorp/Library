9e9df20703b2a49b6e3c8a6b05ea9a0c319f56cb
simorai <109289730+simorai@users.noreply.github.com>
2026-01-12 22:42:54 +0000

    Add layout and navigation components; create home, about, and contact pages

---
+++ resources/views/profile/contact.blade.php
@@ -0,0 +1,7 @@
+<x-layout>
+    <x-slot:heading>
+        Contact Page
+    </x-slot:heading>
+
+    <h1>InovCorp Simao Morais.</h1>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,16 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 })->name('home');
+
+Route::get('/about', function () {
+    return view('profile.about');
+})->name('profile.about');
+
+Route::get('/contact', function () {
+    return view('profile.contact');
+})->name('profile.contact');
 
 Route::middleware([
     'auth:sanctum',
